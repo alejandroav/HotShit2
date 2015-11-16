6e2cb8f10ad5d7ce6ba74bc10d6eb3d858e4496c
Alejandro Alarcón <alejandroalarcon93@gmail.com>
2015-11-16 18:13:04 +0100

    asd

--- weplan.sql
+++ weplan.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `Plan` (
+CREATE TABLE `planes` (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `nombre` VARCHAR(255) NOT NULL,
   `fecha` DATETIME NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE `Plan` (
   `imagen` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `Usuarios` (
+CREATE TABLE `usuarios` (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `nombre` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `Usuarios` (
   `nivelparticipante` DECIMAL(12, 2) NOT NULL
 );
 
-CREATE TABLE `fase` (
+CREATE TABLE `fases` (
   `evento` INTEGER NOT NULL,
   `numfase` INTEGER NOT NULL,
   `coste` DECIMAL(12, 2) NOT NULL,
@@ -27,13 +27,13 @@ CREATE TABLE `fase` (
   PRIMARY KEY (`evento`, `numfase`)
 );
 
-CREATE TABLE `follow` (
+CREATE TABLE `follows` (
   `follower` INTEGER NOT NULL,
   `followed` INTEGER NOT NULL,
   PRIMARY KEY (`follower`, `followed`)
 );
 
-CREATE TABLE `participaen` (
+CREATE TABLE `asistencias` (
   `usuario` INTEGER NOT NULL,
   `evento` INTEGER NOT NULL,
   PRIMARY KEY (`usuario`, `evento`)
