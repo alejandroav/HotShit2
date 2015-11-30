c1674652bd70ba923e468164beb83851df5a5a76
Alejandro Alarcón <alejandroalarcon93@gmail.com>
2015-11-30 17:33:02 +0100

    Android: Creación actividades

---
+++ android-project/WePlan/app/src/main/java/solfamidas/weplan/DetalleEvento.java
@@ -0,0 +1,14 @@
+package solfamidas.weplan;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DetalleEvento extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_evento);
+    }
+
+}
---
+++ android-project/WePlan/app/src/main/java/solfamidas/weplan/ListaEventos.java
@@ -0,0 +1,14 @@
+package solfamidas.weplan;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ListaEventos extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_eventos);
+    }
+
+}
