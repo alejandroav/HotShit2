999d63356215cecad39d83dd7df8b682a0ea32f3
themarioga <marioga2512@gmail.com>
2015-12-11 20:54:40 +0100

    Pequeño update al server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,38 @@
 var io = require('socket.io')(80);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'me',
+	password : 'secret',
+	database : 'my_db'
+});
+connection.connect();
+
+connection.end();
 io.on('connection', function (socket) {
-	socket.on('event-listen', function (data)) {
-		socket.emit('event-send', data);
+	socket.on('c-event-list', function (data)) {
+		connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+			if (err) throw err;
+				socket.emit('s-event-list', data);
+				console.log('The solution is: ', rows[0].solution);
+			});
+	});
+	socket.on('c-event-map', function (data)) {
+		socket.emit('s-event-map', data);
+	});
+	socket.on('c-event-create', function (data)) {
+		socket.emit('s-event-create', data);
+	});
+	socket.on('c-event-edit', function (data)) {
+		socket.emit('s-event-edit', data);
+	});
+	socket.on('c-event-delete', function (data)) {
+		socket.emit('s-event-delete', data);
+	});
+	socket.on('c-event-details', function (data)) {
+		socket.emit('s-event-details', data);
+	});
+	socket.on('c-event-suscribe', function (data)) {
+		socket.emit('s-event-suscribe', data);
 	});
 });
--- weplan.sql
+++ weplan.sql
@@ -1,40 +1,40 @@
 CREATE TABLE `planes` (
-  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `nombre` VARCHAR(255) NOT NULL,
-  `fecha` DATETIME NOT NULL,
-  `descripcion` LONGTEXT NOT NULL,
-  `creador` INTEGER NOT NULL,
-  `imagen` VARCHAR(255) NOT NULL
+	`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+	`nombre` VARCHAR(255) NOT NULL,
+	`fecha` DATETIME NOT NULL,
+	`descripcion` LONGTEXT NOT NULL,
+	`creador` INTEGER NOT NULL,
+	`imagen` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `usuarios` (
-  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `nombre` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` LONGTEXT NOT NULL,
-  `imagen` VARCHAR(255) NOT NULL,
-  `nivelorganizador` DECIMAL(12, 2) NOT NULL,
-  `nivelparticipante` DECIMAL(12, 2) NOT NULL
+	`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL,
+	`email` VARCHAR(100) NOT NULL,
+	`password` VARCHAR(130) NOT NULL,
+	`imagen` VARCHAR(255) NOT NULL,
+	`nivelorganizador` DECIMAL(12, 2) NOT NULL,
+	`nivelparticipante` DECIMAL(12, 2) NOT NULL
 );
 
 CREATE TABLE `fases` (
-  `evento` INTEGER NOT NULL,
-  `numfase` INTEGER NOT NULL,
-  `coste` DECIMAL(12, 2) NOT NULL,
-  `duracion` DECIMAL(12, 2) NOT NULL,
-  `ubicacion` VARCHAR(255) NOT NULL,
-  `descripcion` LONGTEXT NOT NULL,
-  PRIMARY KEY (`evento`, `numfase`)
+	`evento` INTEGER NOT NULL,
+	`numfase` INTEGER NOT NULL,
+	`coste` DECIMAL(12, 2) NOT NULL,
+	`duracion` DECIMAL(12, 2) NOT NULL,
+	`ubicacion` VARCHAR(255) NOT NULL,
+	`descripcion` LONGTEXT NOT NULL,
+	PRIMARY KEY (`evento`, `numfase`)
 );
 
 CREATE TABLE `follows` (
-  `follower` INTEGER NOT NULL,
-  `followed` INTEGER NOT NULL,
-  PRIMARY KEY (`follower`, `followed`)
+	`follower` INTEGER NOT NULL,
+	`followed` INTEGER NOT NULL,
+	PRIMARY KEY (`follower`, `followed`)
 );
 
 CREATE TABLE `asistencias` (
-  `usuario` INTEGER NOT NULL,
-  `evento` INTEGER NOT NULL,
-  PRIMARY KEY (`usuario`, `evento`)
+	`usuario` INTEGER NOT NULL,
+	`evento` INTEGER NOT NULL,
+	PRIMARY KEY (`usuario`, `evento`)
 );
