1ee9147a3e613dce209b4fd6ae632a66915f26f8
themarioga <marioga2512@gmail.com>
2015-12-15 00:12:37 +0100

    press enter to win

--- web-client/index.php
+++ web-client/index.php
@@ -3,10 +3,11 @@
 <html>
 	<head>
 		<title>Grizz.ly - Login</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link type="text/css" rel="stylesheet" href="resources/css/materialize.min.css"	media="screen,projection"/>
 		<link type="text/css" rel="stylesheet" href="resources/css/outter.css"	media="screen,projection"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<script type="text/javascript" src="resources/js/jquery.min.js"></script>
 	</head>
 	<body>
 		<div class="lr-button" id="lr-button">
@@ -52,7 +53,6 @@
 				</li>
 			</ul>
 		</div>
-		<script type="text/javascript" src="resources/js/jquery.min.js"></script>
 		<script type="text/javascript" src="resources/js/jquery.form.min.js"></script>
 		<script type="text/javascript" src="resources/js/materialize.min.js"></script>
 		<script type="text/javascript" src="resources/js/functions.js"></script>
