4981a4f4d8b361d9b481f7893d844a65903eb2b9
themarioga <marioga2512@gmail.com>
2016-01-01 17:36:15 +0100

    jijiji

--- web-client/frontpage.php
+++ web-client/frontpage.php
@@ -5,14 +5,13 @@
 		<title>Grizzly.pw</title>
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link type="text/css" rel="stylesheet" href="resources/css/materialize.min.css"	media="screen,projection"/>
+		<link type="text/css" rel="stylesheet" href="css/materialize.clockpicker.css" media="screen,projection"/>
 		<link type="text/css" rel="stylesheet" href="resources/css/frontpage.css"	media="screen,projection"/>
 		<link type="text/css" rel="stylesheet" href="resources/css/nouislider.min.css"	media="screen,projection"/>
 		<link type="text/css" rel="stylesheet" href="resources/css/nouislider.pips.css"	media="screen,projection"/>
 		<link type="text/css" rel="stylesheet" href="resources/css/nouislider.tooltips.css"	media="screen,projection"/>		
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<script type="text/javascript" src="resources/js/jquery.min.js"></script>
-		<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js"></script>
 		<script src="https://cdn.socket.io/socket.io-1.3.7.js"></script>
 		<script>
 			var socket = io.connect('http://grizzly.pw:8080');
@@ -75,8 +74,11 @@
 		<div id="formcontent"></div>
 		<script type="text/javascript" src="resources/js/jquery.form.min.js"></script>
 		<script type="text/javascript" src="resources/js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/materialize.clockpicker.js"></script>
 		<script type="text/javascript" src="resources/js/functions.js"></script>
 		<script type="text/javascript" src="resources/js/nouislider.min.js"></script>
+		<script type="text/javascript" src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 		<script>
 			var prevPage = "";
 			$(document).ready(function(){
