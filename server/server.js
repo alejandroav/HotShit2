cab5a2405b92beae7c48135cffbb05f917e1693d
themarioga <marioga2512@gmail.com>
2016-01-05 20:07:00 +0100

    cositas varias

--- server/server.js
+++ server/server.js
@@ -23,7 +23,8 @@ io.on('connection', function (socket) {
 		if (('user_id' in data && !isNaN(data.user_id)) && ('session_id' in data && typeof data.session_id == "string")){
 			/*consulta a la bd*/
 			connection.query("SELECT count(*) AS cuantos FROM users WHERE id="+data.user_id+" AND session_id="+mysql.escape(data.session_id), function(err, rows, fields) {
-				if (err) socket.emit('s-login', {status: 'ERROR', query: "SELECT count(*) AS cuantos FROM users WHERE id="+data.user_id+" AND session_id='"+mysql.escape(data.session_id)+"'", msg: err}); //ERROR en la consulta
+				//ERROR en la consulta
+				if (err) socket.emit('s-login', {status: 'ERROR', query: "SELECT count(*) AS cuantos FROM users WHERE id="+data.user_id+" AND session_id='"+mysql.escape(data.session_id)+"'", msg: err});
 				else { //Exito
 					if (rows[0].cuantos > 0) {
 						socket.uid = data.user_id;
@@ -38,45 +39,47 @@ io.on('connection', function (socket) {
 	socket.on('c-event-get', function (data) {
 		if (socket.valid){
 			if (('loc_long' in data && !isNaN(data.loc_long)) && ('loc_lat' in data && !isNaN(data.loc_lat)) && ('max_radio' in data && !isNaN(data.max_radio)) && ('type' in data && !isNaN(data.type))){
-				var extra;
-				if (data.type == 0){
-					extra = "";
-				} else if (data.type == 1){
-					extra = " AND id IN (SELECT event_id FROM attendance WHERE user_id="+socket.uid+") ";
-				} else if (data.type == 2){
-					extra = " AND id IN (SELECT event_id FROM attendance WHERE user_id IN (SELECT follower FROM follows WHERE followed ="+socket.uid+")) ";
-				} else socket.emit('s-event-get', {status: 'ERROR', msg: 'Error en el objeto JSON'});
+				var extraparams = "";
+				var extradata = ",(SELECT count(*) FROM attendance WHERE event_id = id) as current, capacity, image, "+
+					"creator_id, (SELECT username FROM users WHERE id = creator_id) as creator_name, (SELECT count(*) FROM follow WHERE follower="+socket.uid+" AND followed=creator_id) "+
+					"as creator_followed, (SELECT count(*) FROM attendance WHERE event_id=id and user_id="+socket.uid+") as attendance, "+
+					"ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom)/1000 as distance";
+				/*Get params by type*/
+				switch (data.type) {
+					case 0: break;
+					case 1:
+						extraparams = " AND id IN (SELECT event_id FROM attendance WHERE user_id="+socket.uid+") ";
+					break;
+					case 2:
+						extraparams = " AND id IN (SELECT event_id FROM attendance WHERE user_id IN (SELECT follower FROM follows WHERE followed ="+socket.uid+")) ";
+					break;
+					case 3:
+						extradata = "";
+					break;
+					default:
+						socket.emit('s-event-get', {status: 'ERROR', msg: 'Error en el objeto JSON'});
+					break;
+				}
+				/*Extra params*/
 				if (('min_cost' in data && !isNaN(data.min_cost)) && ('max_cost' in data && !isNaN(data.max_cost))){
-					extra += " AND cost >= "+data.min_cost+" AND cost <= "+data.max_cost;
+					extraparams += " AND cost >= "+data.min_cost+" AND cost <= "+data.max_cost;
 				}
 				if (('min_date' in data && typeof data.min_date == "string") && ('max_date' in data && typeof data.max_date == "string")){
-					extra += " AND date >= "+mysql.escape(data.min_date)+" AND date <= "+mysql.escape(data.max_date);
+					extraparams += " AND date >= "+mysql.escape(data.min_date)+" AND date <= "+mysql.escape(data.max_date);
 				}
 				if ('min_radio' in data && !isNaN(data.min_radio)){
-					extra += " AND ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom) >= "+data.min_radio*1000;
+					extraparams += " AND ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom) >= "+data.min_radio*1000;
 				}
-				var query = "SELECT id, title, description, geom, cost, date, (SELECT count(*) FROM attendance WHERE event_id = id) as current, capacity, image, "+
-					"creator_id, (SELECT username FROM users WHERE id = creator_id) as creator_name, (SELECT count(*) FROM follow WHERE follower="+socket.uid+" AND followed=creator_id) "+
-					"as creator_followed, (SELECT count(*) FROM attendance WHERE event_id=id and user_id="+socket.uid+") as attendance, ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom)/1000 as distance FROM events "+
+				/*query*/
+				var query = "SELECT id, title, description, geom, date, cost "+extradata+" FROM events "+
 					"WHERE ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom) <= "+data.max_radio*1000+" "+extra+" ORDER BY distance ASC LIMIT 30";
 				connection.query(query, function(err, rows, fields) {
-					if (err) socket.emit('s-event-get', {status: 'ERROR', msg: err});
+					if (err) socket.emit('s-event-get', {status: 'ERROR', msg: err, query: query});
 					socket.emit('s-event-get', {status: 'OK', data: rows, type: data.type});
 				});
 			} else socket.emit('s-event-get', {status: 'ERROR', msg: 'Error en el objeto JSON'});
 		} else socket.emit('s-event-get', {status: 'ERROR', msg: 'Usuario no valido'});
 	});
-	/*Evento para mapa de eventos*/
-	/*socket.on('c-event-map', function (data) {
-		if (socket.valid){
-			if (('loc_long' in data && !isNaN(data.loc_long)) && ('loc_lat' in data && !isNaN(data.loc_lat)) && ('radio' in data && !isNaN(data.radio))){
-				connection.query("SELECT id, title, description, geom, date FROM events WHERE ST_Distance_Sphere(ST_GeomFromText('POINT("+data.loc_lat+" "+data.loc_long+")'), geom) < "+data.radio*1000, function(err, rows, fields) {
-					if (err) socket.emit('s-event-map', {status: 'ERROR', msg: err});
-					socket.emit('s-event-map', {status: 'OK', data: rows});
-				});
-			} else socket.emit('s-event-map', {status: 'ERROR', msg: 'Error en el objeto JSON'});
-		} else socket.emit('s-event-map', {status: 'ERROR', msg: 'Usuario no valido'});
-	});*/
 	/*Evento para crear eventos*/
 	socket.on('c-event-create', function (data) {
 		if (socket.valid){
@@ -98,7 +101,8 @@ io.on('connection', function (socket) {
 			if (('event_id' in data && !isNaN(data.event_id)) && ('title' in data.data && typeof data.data.title == "string") && ('description' in data.data && typeof data.data.description == "string") 
 				&& ('loc_long' in data.data && !isNaN(data.data.loc_long)) && ('loc_lat' in data.data && !isNaN(data.data.loc_lat)) 
 				&& ('date' in data.data && typeof data.data.date == "string") && ('cost' in data.data && !isNaN(data.data.cost)) && ('capacity' in data.data && !isNaN(data.data.capacity))){
-				connection.query("UPDATE events SET title='"+mysql.escape(xss(data.data.title))+"', description='"+mysql.escape(xss(data.data.description))+"', ST_GeomFromText('POINT("+data.data.loc_lat+" "+data.data.loc_long+")')"+
+				connection.query("UPDATE events SET title='"+mysql.escape(xss(data.data.title))+"', description='"+mysql.escape(xss(data.data.description))+"', "+
+					"ST_GeomFromText('POINT("+data.data.loc_lat+" "+data.data.loc_long+")')"+
 					", date='"+mysql.escape(data.data.date)+"', cost="+data.data.cost+" WHERE id="+data.event_id+" AND creator_id="+socket.uid, function(err, rows, fields) {
 					if (err) socket.emit('s-event-edit', {status: 'ERROR', msg: err});
 					socket.emit('s-event-edit', {status: 'OK'});
